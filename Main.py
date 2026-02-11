06c9b9b07e447b6279f344d966d06a488403a45d
Abdullah Alkaysey <124020544+Alkaysey@users.noreply.github.com>
2026-02-11 15:46:38 -0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,6 +1,13 @@
 #CS 351 Lexler and Parser
 #part 1 due on 2/25/2026
 
+import.re
 
-if __name__ == '__main__':
-    print("Hello World")
+def CutOneLineTokens(z) :
+    y = re.findall("int", z)
+    if len(y) 3=-0:
+    y.append(re.findall("A1", z))
+    print(y)
+def main():
+    print("hey-there")
+CutOneLineTokens ("int A1=5")
