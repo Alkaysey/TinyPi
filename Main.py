2639b85d83be6c9000576650650acc6924c650af
kpgalarno <philias1116@gmail.com>
2026-02-25 14:24:39 -0800

    hi

--- Main.py
+++ Main.py
@@ -2,63 +2,61 @@
 #part 1 due on 2/25/2026
 
 import re
+from enum import Enum
+
+class TokenType(Enum):
+    KEYWORD = str("^(?:int|float|if|for)")
+    IDENTIFIER = str("^[A-Za-z]+[A-Za-z0-9]*")
+    FLOAT = str("^[0-9]{1,}\\.[0-9]{1,}")
+    INTEGER = str("^[0-9]+")
+    STRING = str("^[\"]{1}.*[\"]{1}")
+    SEPERATOR = str("^[()\":]")
+    OPERATOR = str("^[=>+*]")
+
 
 class Token:
     def __init__(self, type, value):
         self.type = type
         self.value = value
     def displayToken(self):
-        print("<",self.type,",",self.value,">")
+        print("<", self.type, "," + self.value + ">")
 
 class Lexer:
     def __init__(self):
         self.arr = []
     def addToken(self, type, value):
         self.arr.append(Token(type, value))
+    def printLine(self):
+        for token in self.arr:
+            token.displayToken()
 
 #patternFind is wip
-def patternFind(string):
-    patterns = ["\b(?:int|float|if|for)\b",]
-    print(string + ' is: ' + "\n")
-    nothing = 0 #if string meets no criteria
-    if re.match("^[0-9]*$", string):
-        print('a. An integer \n')
-
-
-    nothing = nothing + 1
-    if re.match("^\b(?:int|float|if|for)\b", string):
-    print('b. A float consists of 1 or more digits before and after decimal
-    point \n')
-    nothing = nothing + 1
-    if re.match("^[0-9]*\\.[0-9]{2}$", string):
-    print('c. A float with exactly 2 digits after the decimal point \n')
-    nothing = nothing + 1
-    if re.match("^[0-9]*\\.[0-9]*[A-z]", string):
-    nothing = nothing + 1
-    print('d. A float end with letter f (4.321f) \n')
-    if re.match("[A-Z]{1,}[a-z]{1,}\\d{1,}$", string):
-    nothing = nothing + 1
-    print('e. Capital letters, followed by small case letters, followed by
-    digits \n')
-    if re.match("^[0-9]{3}[A-z]{2,}", string):
-    nothing = nothing + 1
-    print('f. Exactly 3 digits, followed by at least 2 letters \n')
-    if nothing == 0:
-    print('Not recognized in any pattern \n')
-
-
 def CutOneLineTokens(string):
-    tokenList = Lexer
+    patterns = ["^(?:int|float|if|for)","^[A-Za-z]+[A-Za-z0-9]*","^[0-9]{1,}\\.[0-9]{1,}","^[0-9]+","^[\"]{1}.*[\"]{1}","^[()\":]","^[=>+*]"]
+    tokenList = Lexer()
+    #print(string)
+    while string != "":
+        for pattern1 in TokenType:
+            #print(string)
 
-    while(string != ""):
-
-        string = string[1:]
-    ranges = re.search(pattern, string)
-    removed = string[ranges.start():ranges.end()]
-    newstring = re.sub(pattern, "", removed)
+            if string[0] == " ":
+                string = re.sub("^\\s*", "", string)
+                #print("space")
+            if re.match(pattern1.value, string):
+                #print(pattern1)
+                ranges = re.search(pattern1.value, string)
+                value1 = string[ranges.start():ranges.end()]
+                #print(value1)
+                tokenList.addToken(pattern1, value1)
+                newString = re.sub(pattern1.value, "", string)
+                string = newString
+                #print(string+" new string")
+                break
     return tokenList
 
 
-def main():
-    print("hey-there")
-CutOneLineTokens ("int A1=5")
+if __name__ == '__main__':
+    #print(CutOneLineTokens("int A1=5"))
+    line = CutOneLineTokens("float A2     =    5.2")
+    line.printLine()
+    print(TokenType.KEYWORD.name)
