de9708beff73871bfa2d53c4a0dcf1cdd18907f2
Keanu Galarneau <philias1116@gmail.com>
2026-02-25 19:37:11 -0800

    konichiwa

--- Main.py
+++ Main.py
@@ -2,45 +2,55 @@
 #part 1 due on 2/25/2026
 
 import re
+from enum import Enum
+
+class TokenType(Enum):
+    KEYWORD = str("^(?:int|float|if|for)")
+    IDENTIFIER = str("^[A-Za-z]+[A-Za-z0-9]*")
+    #LITERAL = str("^([0-9]{1,}\\.[0-9]{1,}|[0-9]+|[\"]{1}.*[\"]{1})")
+    FLOAT = str("^[0-9]{1,}\\.[0-9]{1,}")
+    INTEGER = str("^[0-9]+")
+    STRING = str("^[\"]{1}.*[\"]{1}")
+    SEPERATOR = str("^[()\":]")
+    OPERATOR = str("^[=>+*]")
+
 
 class Token:
     def __init__(self, type, value):
-        self.type = type
+        self.type = TokenType(type)
         self.value = value
     def displayToken(self):
-        print("<",self.type,",",self.value,">")
+        print("<" + self.type.name + "," + self.value + ">")
 
 class Lexer:
     def __init__(self):
         self.arr = []
     def addToken(self, type, value):
         self.arr.append(Token(type, value))
+    def printLine(self):
+        #print(self.arr)
+        for token in self.arr:
+            token.displayToken()
 
-#patternFind is wip
 def CutOneLineTokens(string):
-    patterns = ["^(?:int|float|if|for)","^[A-Za-z]+[A-Za-z0-9]*","^[0-9]{1,}\\.[0-9]{1,}","^[0-9]*","^[\"]{1}.*[\"]{1}","^[()\":]","^[=>+*]"]
     tokenList = Lexer()
-
     while string != "":
-        for pattern1 in patterns:
-            #print(pattern1)
-            print(string)
-            #if re.match("^\s*", string):
-                #string = re.sub(pattern, "", string)
-                #print(string+"2")
-            if re.match(pattern1, string):
-                print(pattern1)
-                ranges = re.search(pattern1, string)
+        for pattern1 in TokenType:
+            if string[0] == " " or string[0] == "\t":
+                string = re.sub("^(\\s*|\\t)", "",string)
+
+            if re.match(pattern1.value, string):
+                ranges = re.search(pattern1.value, string)
                 value1 = string[ranges.start():ranges.end()]
-                print(value1)
                 tokenList.addToken(pattern1, value1)
-                newString = re.sub(pattern1, "", string)
+                newString = re.sub(pattern1.value, "", string)
                 string = newString
-                print(string+"3")
                 break
     return tokenList
 
-
 if __name__ == '__main__':
-    #print(CutOneLineTokens("int A1=5"))
-    CutOneLineTokens("int A2=5")
+    testArr = ["int    A1=5","float BBB2     =1034.2","float     cresult     =     A1     +BBB2     *      BBB2","if(cresult     >10):","    print(\"TinyPie    \"    )"]
+    for currString in testArr:
+        print(currString)
+        line = CutOneLineTokens(currString)
+        line.printLine()
